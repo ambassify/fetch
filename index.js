7caae14a28fdbf7437f90bb28eb59679e43ad96f
GertSallaerts <1267900+GertSallaerts@users.noreply.github.com>
2018-04-27 10:09:44 +0200

    ignore native fetch on Microsoft Edge 14

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Edge 14's fetch implementation is a buggy mess
+ * https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8653298/
+ */
+if (
+    global.navigator &&
+    global.navigator.userAgent &&
+    global.navigator.userAgent.indexOf('Edge/14') > -1
+) delete global.fetch;
+
 // Based on `unfetch` polyfill
 // https://github.com/developit/unfetch/blob/master/packages/isomorphic-unfetch/index.js
 module.exports = global.fetch || (
