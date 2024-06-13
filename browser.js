eb4a502195c61f7d58406c1d73cb5316ce77743f
Gert Sallaerts <1267900+GertSallaerts@users.noreply.github.com>
2024-06-13 16:20:36 +0200

    feat(browser): find global in all modern environments (#11)

--- browser.js
+++ browser.js
@@ -1,12 +1,22 @@
+var _global_ = (function getGlobal() {
+    if (typeof globalThis !== 'undefined')
+        return globalThis;
+
+    if (typeof window !== 'undefined')
+        return window;
+
+    return {};
+})();
+
 /**
  * Edge 14's fetch implementation is a buggy mess
  * https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8653298/
  */
 if (
-    window &&
-    window.navigator &&
-    window.navigator.userAgent &&
-    window.navigator.userAgent.indexOf('Edge/14') > -1
-) delete window.fetch;
+    _global_ &&
+    _global_.navigator &&
+    _global_.navigator.userAgent &&
+    _global_.navigator.userAgent.indexOf('Edge/14') > -1
+) delete _global_.fetch;
 
-module.exports = window.fetch || require('unfetch').default || require('unfetch');
+module.exports = _global_.fetch || require('unfetch').default || require('unfetch');
